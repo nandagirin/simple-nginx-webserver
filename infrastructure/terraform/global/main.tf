f430dcd930be18ffd38520f29d2c4d6a1909f243
nandagirin <nandagirin17@gmail.com>
2025-05-01 22:28:50 +0700

    infra: setup github actions oidc iam

--- infrastructure/terraform/global/main.tf
+++ infrastructure/terraform/global/main.tf
@@ -12,7 +12,7 @@ resource "aws_acm_certificate" "grnnd_online" {
   domain_name       = "grnnd.online"
   validation_method = "DNS"
 
-  subject_alternative_names = [ "*.grnnd.online" ]
+  subject_alternative_names = ["*.grnnd.online"]
 
   tags = {
     Terraform   = "true"
@@ -43,5 +43,52 @@ resource "aws_route53_record" "grnnd_online_cert_validation" {
 
   lifecycle {
     create_before_destroy = true
+  }
+}
+
+module "iam_github_oidc_provider" {
+  source  = "terraform-aws-modules/iam/aws//modules/iam-github-oidc-provider"
+  version = "~> 5.0"
+}
+
+resource "aws_iam_policy" "s3_deploy_policy" {
+  name = "GitHubActionsS3DeployPolicy"
+
+  policy = jsonencode({
+    Version = "2012-10-17",
+    Statement = [
+      {
+        Effect = "Allow",
+        Action = [
+          "s3:PutObject",
+          "s3:PutObjectAcl",
+          "s3:DeleteObject",
+          "s3:ListBucket"
+        ],
+        Resource = [
+          "arn:aws:s3:::webserver-*",
+          "arn:aws:s3:::webserver-*/*"
+        ]
+      }
+    ]
+  })
+}
+
+
+module "iam_github_oidc_role" {
+  source  = "terraform-aws-modules/iam/aws//modules/iam-github-oidc-role"
+  version = "~> 5.0"
+
+  name = "GitHubActionsRole"
+  subjects = ["nandagirin/simple-nginx-webserver:*"]
+
+  policies = {
+    GitHubActionsS3DeployPolicy = aws_iam_policy.s3_deploy_policy.arn
+  }
+
+  tags = {
+    Terraform   = "true"
+    Environment = "global"
+    Context     = "iam"
   }
 }
